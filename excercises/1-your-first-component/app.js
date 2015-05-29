90a5c1296ff7262e78513f71baff238f21afc06d
Cari Westbrook <cari.westbrook@gmail.com>
2015-05-29 11:30:45 -0700

    first exercise

--- excercises/1-your-first-component/app.js
+++ excercises/1-your-first-component/app.js
@@ -23,11 +23,109 @@ var DATA = {
 
 var Menu = React.createClass({
   render () {
-    return null;
+    var mexicanFood = DATA.items.filter(
+      item => item.type === 'mexican'
+    )
+    .sort(sortBy('name'))
+    .map((item) => <li>{item.name}</li>);
+
+    return (
+      <div>
+        <h1>{DATA.title}</h1>
+        <ul>
+          {mexicanFood}
+        </ul>
+      </div>
+    );
   }
 });
 
+
+//////////////////////////////
+//   INSTRUCTOR'S VERSION   //
+//////////////////////////////
+
+// var Menu = React.createClass({
+//   render () {
+//     var mexicanFood = DATA.items.filter((food) => {
+//       return food.type === 'mexican';
+//     })
+//     .sort(sortBy('name'))
+//     .map(function(item) {
+//       return <li>{item.name}</li>
+//     });
+
+//     return (
+//       <div>
+//         <h1>{DATA.title}</h1>
+//         <ul>
+//           {mexicanFood}
+//         </ul>
+//       </div>
+//     );
+//   }
+// });
+
+
 React.render(<Menu/>, document.body, () => {
+  // var meals = DATA.map((n) => <li>{n}</li>);
   require('./tests').run();
 });
 
+
+
+
+
+//////////////////////////////
+//          NOTES           //
+//////////////////////////////
+
+// var { h1, div, ul, li } = React.DOM
+
+// var element = (
+//   div({},
+//     h1({}, "Hello"),
+//     ul({},
+//        li({}, "Cari with a C"),
+//        li({}, "Hunter with an H"),
+//        li({}, "Aly with an A, L, and Y")
+//     )
+//   )
+// );
+
+// var App = React.createClass({
+//   render () {
+//     return (
+//       <div>
+//         <h1>Hello from the Composite</h1>
+//           <ul>
+//             <li>Hello</li>
+//             <li>Goodbye</li>
+//           </ul>
+//       </div>
+//     )
+//   }
+// })
+
+// App = React.createFactory(App);
+
+// React.render(App({}), document.body);
+
+///////////////////////////////////////
+
+// var numbers = ['one', 'two', 'three'];
+
+// var App = React.createClass({
+//   render () {
+//     var items = numbers.map((n) => <li>{n}</li>);
+//     return (
+//       <div>
+//         <ul>
+//           {items}
+//         </ul>
+//       </div>
+//     );
+//   }
+// })
+
+// React.render(<App/>, document.body);
